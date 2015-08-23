bb2c7c06eaa7169c1ceb1429061b104924740215
chweeks <chrisharryweeks@gmail.com>
2015-08-23 12:40:20 +0100

    updated 7-kyu

---
+++ 7-kyu/Greatest common divisor.rb
@@ -0,0 +1,11 @@
+#Description:
+
+#Find the greatest common divisor of two positive integers. 
+#The integers can be large, so you need to find a clever solution.
+
+#The inputs x and y are always greater or equal to 1, 
+#so the the greatest common divisor will always be an integer that is also greater or equal to 1.
+
+def mygcd(x,y)
+  y == 0 ? x : mygcd(y, x%y)
+end
